c715bc33d51acbda03b370a55f3ba54270ed8144
Michelle Wong <michellekwong2@gmail.com>
2020-09-02 17:45:17 -0400

    Folder set up

---
+++ src/app/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Animations App</h1>
+    </div>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app/styles.css';
+import App from './app/index.jsx';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
